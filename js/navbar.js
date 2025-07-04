8f7fdd7c44d43414b742dcbb4237780d6f5c6f6a
Abysimynd <keilieloliveira.2004@gmail.com>
2025-07-04 09:31:39 -0300

    Header
    Message:
    Sessão do header feita
    
    Adicionados:
    - css
    - javascript

---
+++ js/navbar.js
@@ -0,0 +1,22 @@
+function navbarActiveItem() {
+    let navItems = document.querySelectorAll('#header .navbar .nav .nav-link');
+    navItems.forEach(navItem => {
+        navItem.addEventListener('click', (e) => {
+            e.preventDefault;
+            element = e.target;
+
+            if(!element.classList.contains('active-nav-item')) {
+                clearActiveNavbarItens();
+                element.classList.add('active-nav-item');
+            }
+        });
+
+    });
+}
+
+function clearActiveNavbarItens() {
+    let navItems = document.querySelectorAll('#header .navbar .nav .nav-link');
+    navItems.forEach(navItem => {
+        navItem.classList.remove('active-nav-item');
+    })
+}
