0b141bdf70250040b4845123fad90d97721f0046
Abysimynd <keilieloliveira.2004@gmail.com>
2025-07-04 10:16:10 -0300

    Seção sobre
    Message:
    Seção sobre mim adicionada, sem conteudo.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
 window.onload = e => {
 
-    navbarActiveItem();
+navbarActiveItem();
+
+function navbarActiveItem() {
+    let navItems = document.querySelectorAll('#header .navbar .nav .nav-link');
+    navItems.forEach(navItem => {
+        navItem.addEventListener('click', (e) => {
+            e.preventDefault;
+            element = e.target;
+
+            if(!element.classList.contains('active-nav-item')) {
+                clearActiveNavbarItens();
+                element.classList.add('active-nav-item');
+            }
+        });
+
+    });
+}
+
+function clearActiveNavbarItens() {
+    let navItems = document.querySelectorAll('#header .navbar .nav .nav-link');
+    navItems.forEach(navItem => {
+        navItem.classList.remove('active-nav-item');
+    })
+}
 
 }
