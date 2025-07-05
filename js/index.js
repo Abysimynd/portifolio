b409dca60193cba002575304c18e958d374fb038
Abysimynd <keilieloliveira.2004@gmail.com>
2025-07-05 15:16:10 -0300

    Finalizado
    Message:
    O design está bem simplificado, mas funcional, por hora está finalizado.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 window.onload = e => {
 
 navbarActiveItem();
+hasProjects();
+menuControl();
 
 function navbarActiveItem() {
     let navItems = document.querySelectorAll('#header .navbar .nav .nav-link');
@@ -23,6 +25,33 @@ function clearActiveNavbarItens() {
     navItems.forEach(navItem => {
         navItem.classList.remove('active-nav-item');
     })
+}
+
+function hasProjects() {
+    let projects = document.querySelectorAll('#portfolio .projects .box');
+    if(projects.length == 0) {
+        let noProjects = document.getElementById('no-projects');
+        noProjects.style.display = 'flex';
+    }
+}
+
+function menuControl() {
+    let navbar = document.querySelector('#header .navbar')
+    let open = document.getElementById('open-navbar');
+    let close = document.getElementById('close-navbar');
+    open.addEventListener('click', e => {
+        e.preventDefault;
+        e.stopPropagation
+
+        navbar.style.left = 0;
+    });
+
+    close.addEventListener('click', e => {
+        e.preventDefault;
+        e.stopPropagation
+
+        navbar.style.left = '-100%';
+    });
 }
 
 }
